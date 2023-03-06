2579c232e10889013cc02015195eb7de578a03a8
Kasper Holm <kasper.marhauer@gmail.com>
2023-03-06 10:40:17 +0100

    dummy files

---
+++ NewsFilter-4-semester/NewsFilter-4-semester.Library/Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsFilter_4_semester.Library.Models
+{
+    internal class Class1
+    {
+    }
+}
