22f1705194e72dffe0e7bededdf6a95248515823
Kasper Holm <kasper.marhauer@gmail.com>
2023-03-09 13:25:26 +0100

    XML deserialization test

---
+++ NewsFilter-4-semester/NewsFilter-4-semester/Services/XMLReaderService.cs
@@ -0,0 +1,30 @@
+﻿using NewsFilter_4_semester.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using static NewsFilter_4_semester.Models.Article;
+using System.ServiceModel.Syndication;
+
+namespace NewsFilter_4_semester.Services
+{
+    public static class XMLReaderService
+    {
+
+
+        public static Rss Trending()
+        {
+            Rss articles = new();
+            XmlSerializer serializer = new XmlSerializer(typeof(Rss));
+            using (StringReader reader = new StringReader("https://www.dr.dk/nyheder/service/feeds/senestenyt"))
+            {
+                articles = (Rss)serializer.Deserialize(reader);
+            }
+
+            return articles;
+        }
+    }
+}
--- NewsFilter-4-semester/NewsFilter-4-semester/ViewModels/MainViewModel.cs
+++ NewsFilter-4-semester/NewsFilter-4-semester/ViewModels/MainViewModel.cs
@@ -10,26 +10,28 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NewsFilter_4_semester.Services;
 
 namespace NewsFilter_4_semester.ViewModels
 {
     
     public partial class MainViewModel
     {
-        public ObservableCollection<Article.Channel> Articles { get; set; }
+        //public ObservableCollection<Article.Channel> Articles { get; set; }
+        public Article.Rss Articles { get; set; }
 
         public MainViewModel()
         {
-            Articles = new ObservableCollection<Article.Channel>();
-            Articles.Add(new Article.Channel { Title = "title1", Description = "description1" });
-            Articles.Add(new Article.Channel { Title = "title2", Description = "description2" });
-            Articles.Add(new Article.Channel { Title = "title3", Description = "description3" });
-            Articles.Add(new Article.Channel { Title = "title4", Description = "description4" });
-            Articles.Add(new Article.Channel { Title = "title5", Description = "description5" });
-            Articles.Add(new Article.Channel { Title = "title6", Description = "description6" });
-            Articles.Add(new Article.Channel { Title = "title7", Description = "description7" });
-            Articles.Add(new Article.Channel { Title = "title8", Description = "description8" });
-            Articles.Add(new Article.Channel { Title = "title9", Description = "description9" });
+            Articles = XMLReaderService.Trending();
+            //Articles.Add(new Article.Channel { Title = "title1", Description = "description1" });
+            //Articles.Add(new Article.Channel { Title = "title2", Description = "description2" });
+            //Articles.Add(new Article.Channel { Title = "title3", Description = "description3" });
+            //Articles.Add(new Article.Channel { Title = "title4", Description = "description4" });
+            //Articles.Add(new Article.Channel { Title = "title5", Description = "description5" });
+            //Articles.Add(new Article.Channel { Title = "title6", Description = "description6" });
+            //Articles.Add(new Article.Channel { Title = "title7", Description = "description7" });
+            //Articles.Add(new Article.Channel { Title = "title8", Description = "description8" });
+            //Articles.Add(new Article.Channel { Title = "title9", Description = "description9" });
         }
 
         
@@ -39,7 +41,7 @@ namespace NewsFilter_4_semester.ViewModels
             switch (type)
             {
                 case "Trending":
-                    //Articles = XMLReader.Trending();
+                    //Articles = XMLReaderService.Trending();
                     break;
                 case "World":
                     //Articles = XMLReader.World();
