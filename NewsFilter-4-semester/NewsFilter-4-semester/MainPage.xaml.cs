cfe3ce602d70d35af900642443f1ae501a1173ae
Martin Kjeldsen <wcgamers13@gmail.com>
2023-03-09 09:23:57 +0100

    MVVM setup

--- NewsFilter-4-semester/NewsFilter-4-semester/MainPage.xaml.cs
+++ NewsFilter-4-semester/NewsFilter-4-semester/MainPage.xaml.cs
@@ -1,29 +1,18 @@
 ﻿using Microsoft.Maui.Controls;
 using NewsFilter_4_semester.Models;
+using NewsFilter_4_semester.ViewModels;
 using System.Collections.ObjectModel;
 
 namespace NewsFilter_4_semester
 {
     public partial class MainPage : ContentPage
     {
-        public ObservableCollection<Article.Channel> Articles { get; set; }
 
-        public MainPage()
+        public MainPage(MainViewModel model)
         {
             InitializeComponent();
 
-            Articles = new ObservableCollection<Article.Channel>();
-            Articles.Add(new Article.Channel { Title = "title1", Description = "description1" });
-            Articles.Add(new Article.Channel { Title = "title2", Description = "description2" });
-            Articles.Add(new Article.Channel { Title = "title3", Description = "description3" });
-            Articles.Add(new Article.Channel { Title = "title4", Description = "description4" });
-            Articles.Add(new Article.Channel { Title = "title5", Description = "description5" });
-            Articles.Add(new Article.Channel { Title = "title6", Description = "description6" });
-            Articles.Add(new Article.Channel { Title = "title7", Description = "description7" });
-            Articles.Add(new Article.Channel { Title = "title8", Description = "description8" });
-            Articles.Add(new Article.Channel { Title = "title9", Description = "description9" });
-
-            BindingContext = this;
+            BindingContext = model;
         }
     }
 }
--- NewsFilter-4-semester/NewsFilter-4-semester/ViewModels/MainViewModel.cs
+++ NewsFilter-4-semester/NewsFilter-4-semester/ViewModels/MainViewModel.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using NewsFilter_4_semester.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NewsFilter_4_semester.ViewModels
 {
+    
+
     public class MainViewModel
     {
+        public ObservableCollection<Article.Channel> Articles { get; set; }
+        public MainViewModel()
+        {
+            Articles = new ObservableCollection<Article.Channel>();
+            Articles.Add(new Article.Channel { Title = "title1", Description = "description1" });
+            Articles.Add(new Article.Channel { Title = "title2", Description = "description2" });
+            Articles.Add(new Article.Channel { Title = "title3", Description = "description3" });
+            Articles.Add(new Article.Channel { Title = "title4", Description = "description4" });
+            Articles.Add(new Article.Channel { Title = "title5", Description = "description5" });
+            Articles.Add(new Article.Channel { Title = "title6", Description = "description6" });
+            Articles.Add(new Article.Channel { Title = "title7", Description = "description7" });
+            Articles.Add(new Article.Channel { Title = "title8", Description = "description8" });
+            Articles.Add(new Article.Channel { Title = "title9", Description = "description9" });
+        }
     }
 }
